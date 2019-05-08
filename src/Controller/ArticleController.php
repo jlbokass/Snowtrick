01e3a2b1fccab910de15bbf114db0de38902cf76
jlbokass <j_dabok@me.com>
2019-05-08 15:04:51 +0200

    Adding sec-checker

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ArticleController.php',
+        ]);
+    }
+}
