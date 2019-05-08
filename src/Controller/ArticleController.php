1d7d18f96bba0e60cda908308ff15da712aac760
jlbokass <j_dabok@me.com>
2019-05-08 23:34:45 +0200

    render view with twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,20 +12,51 @@ class ArticleController extends AbstractController
     /**
      * @Route("/",name="app_homepage")
      */
-    public function homepage()
+    public function homepage(ArticleRepository $articleRepository)
     {
+        /** @var Article $articles */
+        $articles = $articleRepository->findAll();
+
         return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
+            'articles' => $articles,
+        ]);
+    }
+
+    /**
+     * @Route("/articles", name="app_articles")
+     */
+    public function article(ArticleRepository $articleRepository)
+    {
+        $articles = $articleRepository->findAll();
+
+        return $this->render('article/article.html.twig', [
+            'articles' => $articles
         ]);
     }
 
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show($slug)
+    public function show($slug, ArticleRepository $articleRepository)
     {
+        /** @var Article $articles */
+        $article = $articleRepository->findOneBy(['slug' => $slug]);
+
+        if (!$article) {
+
+            throw $this->createNotFoundException(sprintf('No article for slug "%s"', $slug));
+        }
+
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on all-asteroid diet!',
+            'I like bacon too! Buy some from my site! backinsomebacon.com'
+        ];
+
         return $this->render('article/show.html.twig', [
-            'slug' => $slug
+            'slug' => $slug,
+            'article' => $article,
+            'comments' => $comments,
         ]);
     }
 }
