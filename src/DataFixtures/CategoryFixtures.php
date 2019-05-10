5910083aad0b099f7d89ce3fb50fa3b9d2624033
jlbokass <j_dabok@me.com>
2019-05-10 03:16:46 +0200

    Adding category fixture

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -2,15 +2,27 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Category;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class CategoryFixtures extends Fixture
+class CategoryFixtures extends BaseFixture
 {
-    public function load(ObjectManager $manager)
+    private static $categoryTitle = [
+        'Straight airs',
+        'Grabs',
+        'Spins',
+        'Flips and inverted rotations',
+        'Inverted hand plants',
+        'Slides',
+    ];
+
+    public function loadData(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->createMany(Category::class,6, function (Category $category) {
+
+            $category->setCategoryName($this->faker->unique()->randomElement(self::$categoryTitle));
+            $category->setCreatedAt($this->faker->dateTimeBetween('-100 days', '-1 days'));
+        });
 
         $manager->flush();
     }
