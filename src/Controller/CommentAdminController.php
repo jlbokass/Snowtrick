66ef4b975750869056e137997813d47dd478dbe8
jlbokass <j_dabok@me.com>
2019-05-10 02:18:10 +0200

    Adding search in comment admin

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommentAdminController extends AbstractController
@@ -10,10 +12,14 @@ class CommentAdminController extends AbstractController
     /**
      * @Route("/admin/comment", name="comment_admin")
      */
-    public function index()
+    public function index(CommentRepository $commentRepository, Request $request)
     {
+        $q = $request->query->get('q');
+
+        $comments = $commentRepository->findAllWithSearch($q);
+
         return $this->render('comment_admin/index.html.twig', [
-            'controller_name' => 'CommentAdminController',
+            'comments' => $comments,
         ]);
     }
 }
