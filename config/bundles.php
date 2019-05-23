d3e1fc737bff6fdef393adeb062bcf481f92a3ad
jlbokass <j_dabok@me.com>
2019-05-23 11:36:58 +0200

    Adding Webpack Encore

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
